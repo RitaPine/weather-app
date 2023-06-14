cb094b73f0c68d3693c038c9568c7149bbaf9ef3
RitaPine <116173507+RitaPine@users.noreply.github.com>
2023-06-14 16:35:27 +0100

    updated wind and humidity

--- src/style.js
+++ src/style.js
@@ -1,11 +1,16 @@
 function displayTemperature(response) {
-  console.log(response.data.condition.description);
+  console.log(response.data.wind);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "2fffe23940fcdata45965eb10o0309a4";
